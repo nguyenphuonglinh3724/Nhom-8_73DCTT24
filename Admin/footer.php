0df913ca3d660fc9e230fd7b4615cbce88be68a9
nguyenphuonglinh3724 <kbtnot@gmail.com>
2025-04-16 16:04:09 +0700

    Delete Admin directory

--- Admin/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-        <div class="pull-right hidden-xs">
-          <b>Copyright &copy;</b> 2024
-        </div>
-        <strong>Đồ án lập trình web nâng cao - Nhóm 7</strong>
